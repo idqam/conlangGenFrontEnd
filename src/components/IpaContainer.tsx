0418babfa44798d8f6da7385e136c543dc1b3a9e
idqam <owenjave5@gmail.com>
2025-02-21 21:56:54 -0500

    text

--- src/components/IpaContainer.tsx
+++ src/components/IpaContainer.tsx
@@ -7,7 +7,7 @@ export const IpaContainer = () => {
       <h1 className="text-3xl font-bold text-gray-800 mb-8 p-4 bg-blue-400 rounded-lg shadow-md">
         IPA Selector
       </h1>
-      <div className="flex flex-col md:flex-row justify-center gap-8 w-full max-w-6xl">
+      <div className="flex flex-col md:flex-row justify-center gap-7 w-full max-w-6xl">
         <IpaButtonGrid symbols={IPA_CONSONANTS} header="Consonants" />
         <IpaButtonGrid symbols={IPA_VOWELS} header="Vowels" />
       </div>
